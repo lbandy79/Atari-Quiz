dbfbd1b6b9a9f821d2e8463af3b01fe18a48640d
Luke Bandy <luke.bandy@gmail.com>
2018-04-12 19:20:54 -0400

    Restyled the Quiz

--- app.js
+++ app.js
@@ -174,28 +174,30 @@ $(document).ready(() => {
     const previousButton = document.getElementById("previous");
 	const nextButton = document.getElementById("next");
 	const slides = document.querySelectorAll(".slide");
+
 	let currentSlide = 0;
 
-	function showSlide(n) {
-  		slides[currentSlide].classList.remove('active-slide');
-  		slides[n].classList.add('active-slide');
-  		currentSlide = n;
-  		if(currentSlide===0){
-    		previousButton.style.display = 'none';
-  		}
-  		else{
-    		previousButton.style.display = 'inline-block';
-  		}		
-  		if(currentSlide===slides.length-1){
-    		nextButton.style.display = 'none';
-    		submitButton.style.display = 'inline-block';
-  		}
-  		else{
-    		nextButton.style.display = 'inline-block';
-    		submitButton.style.display = 'none';
-  			}
-		}
-		showSlide(0);
+    function showSlide(n) {
+        slides[currentSlide].classList.remove('active-slide');
+        slides[n].classList.add('active-slide');
+        currentSlide = n;
+        if(currentSlide===0){
+            previousButton.style.display = 'none';
+        }
+        else{
+            previousButton.style.display = 'inline-block';
+        }       
+        if(currentSlide===slides.length-1){
+            nextButton.style.display = 'none';
+            submitButton.style.display = 'inline-block';
+        }
+        else{
+            nextButton.style.display = 'inline-block';
+            submitButton.style.display = 'none';
+            }
+        }
+        showSlide(0);
+
 
 	function showNextSlide() {
   		showSlide(currentSlide + 1);
@@ -209,8 +211,8 @@ $(document).ready(() => {
 	boxSlide(boxIndex);	
 
 	function boxSlide(o) {
-  		var i;
-  		var box = document.getElementsByClassName("boxart");
+  		let i;
+  		let box = document.getElementsByClassName("boxart");
   		if (o > box.length) {boxIndex = 1} 
   		if (o < 1) {boxIndex = box.length}
   		for (i = 0; i < box.length; i++) {
